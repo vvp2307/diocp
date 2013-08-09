9afd0b7aba8f8cc35c2c63721d767047069eb5a7
Mofen.Yang@gmail.com <Mofen.Yang@gmail.com@3a6a21d5-2597-1d0e-8daa-590c91efa5af>
2013-08-09 06:12:22 +0000

    添加ICS, ECHO demo

---
+++ Demos/Echo/ICSClient/IOCPICSClient.dpr
@@ -0,0 +1,32 @@
+program IOCPICSClient;
+
+uses
+  FastMM4,
+  FastMM4Messages,
+  Forms,
+  ufrmMain in 'ufrmMain.pas' {frmMain},
+  uSocketTools in '..\..\..\Source\Utils\uSocketTools.pas',
+  uMemoLogger in '..\..\..\Source\Utils\uMemoLogger.pas',
+  uEchoTester in 'uEchoTester.pas',
+  JSonStream in '..\..\Common\JSonStream.pas',
+  uNetworkTools in '..\..\IOCPCoder\uNetworkTools.pas',
+  uJSonStreamTools in '..\..\Common\uJSonStreamTools.pas',
+  uCRCTools in '..\..\Common\uCRCTools.pas',
+  superobject in '..\..\Common\superobject.pas',
+  JwaWinsock2 in '..\..\..\Source\WinSock2\JwaWinsock2.pas',
+  JwaMSWSock in '..\..\..\Source\WinSock2\JwaMSWSock.pas',
+  JwaQos in '..\..\..\Source\WinSock2\JwaQos.pas',
+  FileLogger in '..\..\..\Source\IOCP\FileLogger.pas',
+  uIOCPProtocol in '..\..\..\Source\IOCP\uIOCPProtocol.pas',
+  uTesterTools in '..\..\IOCPCoder\uTesterTools.pas',
+  uICSClientJSonStreamCoder in '..\..\IOCPCoder\uICSClientJSonStreamCoder.pas',
+  uBuffer in '..\..\..\Source\IOCP\uBuffer.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
