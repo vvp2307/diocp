8b3578f3cd63982dc78f3942fb4f553b4e2a032d
Mofen.Yang@gmail.com <Mofen.Yang@gmail.com@3a6a21d5-2597-1d0e-8daa-590c91efa5af>
2014-03-01 02:25:18 +0000

    TMyUniQuery = class(TUniQuery)   protected     /// <summary>     ///   覆盖掉，避免TDataSetProvider.data时执行PSReset时重复刷新     /// </summary>     procedure PSReset;override;

--- trunk/Demos/UniDACPool/uCDSProvider.pas
+++ trunk/Demos/UniDACPool/uCDSProvider.pas
@@ -3,7 +3,7 @@ unit uCDSProvider;
 interface
 
 uses
-  DBClient, Provider, SysUtils, ActiveX, Uni;
+  DBClient, Provider, SysUtils, ActiveX, Uni, DB;
 
 type
   TMyUniQuery = class(TUniQuery)
@@ -33,6 +33,10 @@ type
     //获取一个CDS.XMLDATA数据包
     function QueryXMLData(pvCmdText: string): string;
 
+    function Query(pvCmdText: string; pvOperaMsg: string = ''): TDataSet;
+
+    function createQuery(pvCmdText: string; pvOperaMsg: string = ''): TDataSet;
+
     procedure ExecuteScript(pvCmdText:String; pvOperaMsg: string = '');
 
     property Connection: TUniConnection read FConnection write SetConnection;
@@ -48,10 +52,28 @@ begin
   FProvider := TDataSetProvider.Create(nil);
   FProvider.Options := FProvider.Options + [poIncFieldProps];
 
-  FQuery := TUniQuery.Create(nil);
+  FQuery := TMyUniQuery.Create(nil);
+
+
   FQuery.DisableControls;
   FQuery.ParamCheck := false;
   FProvider.DataSet := FQuery;
+end;
+
+function TCDSProvider.createQuery(pvCmdText, pvOperaMsg: string): TDataSet;
+begin
+  Result := TUniQuery.Create(nil);
+  try
+    TUniQuery(Result).Connection := FConnection;
+    TUniQuery(Result).Close;
+    TUniQuery(Result).SQL.Clear;
+    TUniQuery(Result).SQL.Add(pvCmdText);
+    TUniQuery(Result).Open;
+  except
+    Result.Free;
+    Result := nil;
+    raise;
+  end;
 end;
 
 destructor TCDSProvider.Destroy;
@@ -69,6 +91,21 @@ begin
     FQuery.SQL.Clear;
     FQuery.SQL.Add(pvCmdText);
     FQuery.ExecSQL;
+  except on e: Exception do
+    begin
+       raise;
+    end;
+  end;
+end;
+
+function TCDSProvider.Query(pvCmdText, pvOperaMsg: string): TDataSet;
+begin
+  try
+    FQuery.Close;
+    FQuery.SQL.Clear;
+    FQuery.SQL.Add(pvCmdText);
+    FQuery.Open;
+    Result := FQuery;
   except on e: Exception do
     begin
        raise;
@@ -128,8 +165,6 @@ end;
 
 procedure TMyUniQuery.PSReset;
 begin
-  //inherited PSReset;
-  
 end;
 
 end.
